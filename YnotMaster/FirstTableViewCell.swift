914ae0a53e096b6e124c12cd85caa428190f85e8
ynot16 <695110728@qq.com>
2016-06-13 16:06:37 +0800

    first commit

---
+++ YnotMaster/FirstTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstTableViewCell.swift
+//  YnotMaster
+//
+//  Created by bori－applepc on 16/6/12.
+//  Copyright © 2016年 bori－applepc. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ YnotMaster/UISliderImageView.swift
@@ -0,0 +1,21 @@
+//
+//  UISliderImageView.swift
+//  YnotMaster
+//
+//  Created by bori－applepc on 16/6/7.
+//  Copyright © 2016年 bori－applepc. All rights reserved.
+//
+
+import UIKit
+
+class UISliderImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
