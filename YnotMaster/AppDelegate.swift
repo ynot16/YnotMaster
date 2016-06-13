574a173ede8e12b878f8f14bc575dea7e299863e
ynot16 <695110728@qq.com>
2016-06-13 17:39:35 +0800

    another test

--- YnotMaster/AppDelegate.swift
+++ YnotMaster/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        print("test if it show up on the test branch")
+        print("tesasdasdasdsat if it show up on the test branch")
         return true
     }
 
