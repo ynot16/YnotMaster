906f605475cc9fa1f22c716c5ad3d3f73447e6ad
ynot16 <695110728@qq.com>
2016-06-13 20:52:36 +0800

    add things in the test branch

--- YnotMaster/AppDelegate.swift
+++ YnotMaster/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         print("saasdsat if it show up on the test branch")
+        print("this is written in the test branch")
         return true
     }
 
